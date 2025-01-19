3eb4182aeb4c5e3db8c82db44cb4a4ff9fb9e675
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-01-19 13:05:07 -0500

    songs page css

--- react-vite/src/components/SongsComponent/SongsComponent.jsx
+++ react-vite/src/components/SongsComponent/SongsComponent.jsx
@@ -4,12 +4,13 @@ import { useEffect } from 'react';
 import { thunkAllSongs, thunkUserSongs } from '../../redux/songs';
 import { thunkAllLikes } from '../../redux/likes';
 import SongTile from './SongTileComponent';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 
 export default function SongsComponent() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const user = useSelector(state=>state.session.user);
 
     useEffect(() => {
@@ -24,26 +25,47 @@ export default function SongsComponent() {
     if(allSongs) otherSongs = Object.values(allSongs).filter(list=>list?.owner_id !== user?.id);
     
     const handleClick = e => {
-        e.preventDefault()
+        e.preventDefault();
+        navigate('/songs/new')
     }
 
     if(user) { return(
         <>
         <h1>Songs</h1>
-        <div>
-           <h2>Your Songs</h2>
-            <button onClick={handleClick}><Link to='/songs/new'>upload a track</Link> </button>
+        <div className='songs-page'>
+           <div className='title-button' id='songs-page'>
+            <h2>Your Songs</h2>
+                <button onClick={handleClick}>upload a track</button>
+            </div>
+            <div className='songs-container' id='your-songs'>
+               {userSongs && 
+               Object.values(userSongs)?.map(song=>
+               <SongTile key={song.id} song={song} user={user} 
+               />)} 
+            </div>
+            
+            <h2>User Submissions</h2>
+            <div className='songs-container' id='user-songs'>
+            {otherSongs && 
+            otherSongs?.map(song=>
+            <SongTile key={song.id} song={song} user={user} 
+            />)} 
+            </div>
+            
         </div>
-        {userSongs && Object.values(userSongs)?.map(song=><SongTile key={song.id} song={song} user={user} />)}
-        <h2>User Submissions</h2>
-        {otherSongs && otherSongs?.map(song=><SongTile key={song.id} song={song} user={user} />)}
+        
         </>
         )}
     
         else return (
-        <>
+        <div className='songs-page'>
         <h1>Songs</h1>
-        {allSongs && Object.values(allSongs)?.map(song=><SongTile key={song.id} song={song} user={user} />)}
-        </>
+        <div className='songs-container'>
+          {allSongs && 
+          Object.values(allSongs)?.map(song=>
+          <SongTile key={song.id} song={song} user={user} 
+          />)}  
+        </div>
+        </div>
         )
 }
