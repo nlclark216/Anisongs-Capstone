7a115a0a5c78ac896c4d47c1f0f86d8a3ecb94fb
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-01-22 10:23:03 -0500

    separating music and image files

--- app/forms/song_form.py
+++ app/forms/song_form.py
@@ -3,7 +3,7 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, SelectField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Songs
-from app.api.helper import ALLOWED_EXTENSIONS
+from app.api.helper import ALLOWED_MUSIC_EXTENSIONS
 
 def song_exists(form, field):
     # Checking if song exists
@@ -15,7 +15,7 @@ def song_exists(form, field):
 class SongForm(FlaskForm):
     title = StringField('title', validators=[DataRequired(), song_exists])
     artist = StringField('artist', validators=[DataRequired()])
-    song_file = FileField("song_file", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    song_file = FileField("song_file", validators=[FileRequired(), FileAllowed(list(ALLOWED_MUSIC_EXTENSIONS))])
     song_img = StringField('song_img', validators=[])
     anime = StringField('artist', validators=[DataRequired()])
     album_name = StringField('artist', validators=[DataRequired()])
