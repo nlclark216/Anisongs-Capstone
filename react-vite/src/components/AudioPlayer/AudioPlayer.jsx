e0377d2b454cd60ef7a4b3bb5fdc23a12596e5b3
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-02-23 15:56:05 -0500

    react audio player integrated

--- react-vite/src/components/AudioPlayer/AudioPlayer.jsx
+++ react-vite/src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,37 +1,17 @@
-import { useEffect, useState } from 'react';
-import { useGlobalAudioPlayer } from 'react-use-audio-player';
-import { FaPlay, FaPause } from "react-icons/fa";
+
+import ReactAudioPlayer from 'react-audio-player';
 import './AudioPlayer.css';
 
 
 export default function AudioPlayer({url}) {
-    
-    
-    const { load, play, pause } = useGlobalAudioPlayer();
-    const [playing, setPlaying] = useState(false);
-
-    const toggle = () => {
-        setPlaying(!playing);
-        if (playing === true) return pause();
-            else return play();
-    }
-    
-    useEffect(() => {
-        load(`${url}`, {
-        autoplay: false,
-        html5: true,
-        format: 'mp3'
-      });
-        
-    }, [load, url])
 
     return (
-        <div>
-            <button className='audio-button' onClick={toggle}>{playing ? <span className='play-button'><FaPause />Pause</span> : <span className='play-button'><FaPlay />Play</span>}</button>
-            {playing && 
-            <div id="container">
-                <div id="html-spinner"></div>
-            </div>}
+        <div className='music-player-container'>
+            <ReactAudioPlayer
+            src={url}
+            controls
+            className='music-player' 
+            />
         </div>
     )
 }
