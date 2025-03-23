69f409548fe614d314b0a594c06e92905544d48b
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-03-23 09:08:41 -0400

    added signup form validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, PasswordField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -20,13 +20,13 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    first_name = StringField('first_name', validators=[DataRequired()])
-    username = StringField('username', validators=[DataRequired(), username_exists])
+    first_name = StringField('first_name', validators=[DataRequired(), Length(min=1,max=50,message='Please provide the first 50 characters of your first name')])
+    username = StringField('username', validators=[DataRequired(), username_exists, Length(min=1,max=50,message='Username must be between 1 and 50 characters long')])
     email = StringField('email', validators=[DataRequired(), user_exists, Email()])
-    password = PasswordField('password', validators=[DataRequired()])
-    last_name = StringField('last_name', validators=[DataRequired()])
-    city = StringField('city', validators=[DataRequired()])
-    state = StringField('state', validators=[DataRequired()])
-    country = StringField('country', validators=[], default='')
+    password = PasswordField('password', validators=[DataRequired(), Length(min=6,max=50,message='Password must be between 6 and 50 characters long')])
+    last_name = StringField('last_name', validators=[DataRequired(), Length(min=1,max=50,message='Please provide the first 50 characters of your last name')])
+    city = StringField('city', validators=[DataRequired(), Length(min=1, max=50, message='Please provide your city name')])
+    state = StringField('state', validators=[DataRequired(), Length(min=2, max=50, message='Please provide your state')])
+    country = StringField('country', validators=[Length(min=2,max=50)], default='')
     address = StringField('address', validators=[])
     profile_pic = StringField('profile_pic', validators=[])
