4624650c3a70b8ed8aa9ff9a1cae262f8cb4729b
Nia C <157457101+nlclark216@users.noreply.github.com>
2025-03-23 09:24:53 -0400

    added song form validators

--- app/forms/song_form.py
+++ app/forms/song_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SelectField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, Length, NumberRange
 from app.models import Songs
 from app.api.helper import ALLOWED_MUSIC_EXTENSIONS
 
@@ -13,11 +13,11 @@ def song_exists(form, field):
         raise ValidationError('Song with that title already in system.')
     
 class SongForm(FlaskForm):
-    title = StringField('title', validators=[DataRequired(), song_exists])
-    artist = StringField('artist', validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired(), song_exists, Length(min=1,max=50)])
+    artist = StringField('artist', validators=[DataRequired(), Length(min=1,max=50)])
     song_file = FileField("song_file", validators=[FileRequired(), FileAllowed(list(ALLOWED_MUSIC_EXTENSIONS))])
-    anime = StringField('artist', validators=[DataRequired()])
-    album_name = StringField('artist', validators=[DataRequired()])
+    anime = StringField('artist', validators=[DataRequired(), Length(min=1,max=50)])
+    album_name = StringField('artist', validators=[DataRequired(), Length(min=1,max=50)])
     album_art = StringField('artist', validators=[])
-    year = IntegerField('year', validators=[DataRequired()])
+    year = IntegerField('year', validators=[DataRequired(), NumberRange(min=1900,max=2050,message='Must provide valid album year')])
     submit = SubmitField("Upload Song")
